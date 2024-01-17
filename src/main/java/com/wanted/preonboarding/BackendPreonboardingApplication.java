b0cd063e19bee84c118003d4a45e733d5ae35760
bingbong-party <ww9720@gmail.com>
2024-01-17 23:03:17 +0900

    test

--- src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
+++ src/main/java/com/wanted/preonboarding/BackendPreonboardingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendPreonboardingApplication {
 
-    // commit test
     public static void main(String[] args) {
         SpringApplication.run(BackendPreonboardingApplication.class, args);
     }
